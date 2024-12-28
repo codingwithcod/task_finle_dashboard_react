cdf805df4024e997cb6a9f8252d7c8a23ca24dd3
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-28 22:31:13 +0530

    update not found page

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -2,17 +2,19 @@ import { Link } from "react-router";
 
 const NotFound = () => {
   return (
-    <div className="flex min-h-screen items-center px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+    <div className="flex min-h-screen items-center bg-gradient-to-br from-deepBlack to-[#02071d] px-4 py-12 text-forground sm:px-6 md:px-8 lg:px-12 xl:px-16">
       <div className="w-full space-y-6 text-center">
         <div className="space-y-3">
           <h1 className="animate-bounce text-4xl font-bold tracking-tighter sm:text-5xl">404</h1>
-          <p className="text-gray-500">
+          <p className="text-gray-300">
             Looks like you've ventured into the unknown digital realm.
           </p>
         </div>
 
         <Link to={"/"}>
-          <button className="mt-5">Return to website</button>
+          <button className="mt-5 h-10 w-40 rounded-md border border-white font-semibold text-white duration-500 hover:bg-white/90 hover:text-black">
+            Return to website
+          </button>
         </Link>
       </div>
     </div>
