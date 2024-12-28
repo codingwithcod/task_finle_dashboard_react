808950c12ebb0b08c5e8549efc34a2a421bba7f8
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-28 22:28:10 +0530

    design side bar for dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,59 @@
+import { AiFillHome } from "react-icons/ai";
+import { FaMoneyCheckAlt } from "react-icons/fa";
+import { HiClipboardDocumentCheck } from "react-icons/hi2";
+import { IoSettings } from "react-icons/io5";
+import { MdAnalytics } from "react-icons/md";
+import { NavLink, useLocation } from "react-router-dom";
+
+const navlinks = [
+  {
+    title: "Home",
+    url: "/dashboard",
+    icon: <AiFillHome />,
+  },
+  {
+    title: "Analytics",
+    url: "/dashboard/analytics",
+    icon: <MdAnalytics />,
+  },
+  {
+    title: "Report",
+    url: "/dashboard/report",
+    icon: <HiClipboardDocumentCheck />,
+  },
+  {
+    title: "Finance",
+    url: "/dashboard/finance",
+    icon: <FaMoneyCheckAlt />,
+  },
+  {
+    title: "Settings",
+    url: "/dashboard/settings",
+    icon: <IoSettings />,
+  },
+];
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const location = useLocation();
+
+  return (
+    <div className="flex flex-col gap-1 text-forground">
+      {navlinks.map((item) => {
+        const { url, icon } = item;
+        return (
+          <NavLink to={url}>
+            <div className="flex h-10 w-full items-center justify-center text-2xl duration-300 hover:bg-primary/10">
+              <div></div>
+              {location.pathname === url && (
+                <div className="absolute left-0 h-7 w-1 rounded-r-sm bg-primary"></div>
+              )}
+              {icon}
+            </div>
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Sidebar;
