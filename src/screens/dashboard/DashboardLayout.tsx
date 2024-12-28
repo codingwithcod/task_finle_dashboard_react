998343d8b296f6f094d63b4fa3cd9e1a7637d5d3
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-28 21:37:54 +0530

    design dashboard layout

--- src/screens/dashboard/DashboardLayout.tsx
+++ src/screens/dashboard/DashboardLayout.tsx
@@ -1,5 +1,24 @@
+import Sidebar from "@/components/Sidebar";
+import { Outlet } from "react-router-dom";
+
 const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+  return (
+    <div className="flex h-screen w-full">
+      <div className="flex h-full w-full flex-col bg-white">
+        <div className="h-[5rem] w-full bg-background"></div>
+        <div className="flex h-[calc(100%-5rem)] w-full bg-yellow-200">
+          {/* ---> Sidebar  */}
+          <aside className="h-full w-[5rem] bg-background">
+            <Sidebar />
+          </aside>
+          {/* ---> Main Content  */}
+          <main className="h-full w-full overflow-y-auto bg-deepBlack p-5">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
--- src/screens/dashboard/dashboardHome/DashboardHome.tsx
+++ src/screens/dashboard/dashboardHome/DashboardHome.tsx
@@ -1,5 +1,20 @@
 const DashboardHome = () => {
-  return <div>DashboardHome</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-5">
+        {[...new Array(10)].map(() => {
+          return (
+            <div className="h-60 rounded-md bg-background p-4 text-forground">
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, hic.</h1>
+              <h1 className="text-primary">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, hic.
+              </h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default DashboardHome;
