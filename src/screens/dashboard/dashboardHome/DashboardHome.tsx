476efe8f6fb33e9ac0eecf162fc9b04bd98617e4
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-29 12:53:47 +0530

    design dashboard home page responsive layout

--- src/screens/dashboard/dashboardHome/DashboardHome.tsx
+++ src/screens/dashboard/dashboardHome/DashboardHome.tsx
@@ -1,17 +1,31 @@
 const DashboardHome = () => {
   return (
-    <div>
-      <div className="grid grid-cols-4 gap-5">
-        {[...new Array(10)].map(() => {
-          return (
-            <div className="h-60 rounded-md bg-background p-4 text-forground">
-              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, hic.</h1>
-              <h1 className="text-primary">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, hic.
-              </h1>
-            </div>
-          );
-        })}
+    <div className="text-forground">
+      <h1 className="mb-5 text-2xl font-semibold">Dashboard</h1>
+      {/* ---> Main container */}
+      <div className="flex flex-col gap-5">
+        {/* ---> Kpi's */}
+        <div className="flex flex-col gap-5 lg:flex-row">
+          <div className="grid w-full grid-cols-4 gap-3 lg:w-[65%]">
+            <div className="h-28 rounded-md bg-background"></div>
+            <div className="h-28 rounded-md bg-background"></div>
+            <div className="h-28 rounded-md bg-background"></div>
+            <div className="h-28 rounded-md bg-background"></div>
+          </div>
+          <div className="h-28 w-full rounded-md bg-red-500/50 lg:w-[35%]"></div>
+        </div>
+
+        {/* ---> Activity Graph */}
+        <div className="flex flex-col gap-5 lg:flex-row">
+          <div className="h-52 w-full rounded-md bg-blue-500/50 lg:w-[65%]"></div>
+          <div className="h-52 w-full rounded-md bg-red-500/50 lg:w-[35%]"></div>
+        </div>
+
+        {/* ---> Recent orders and feedback. */}
+        <div className="flex flex-col gap-5 lg:flex-row">
+          <div className="h-52 w-full rounded-md bg-blue-500/50 lg:w-[65%]"></div>
+          <div className="h-52 w-full rounded-md bg-red-500/50 lg:w-[35%]"></div>
+        </div>
       </div>
     </div>
   );
