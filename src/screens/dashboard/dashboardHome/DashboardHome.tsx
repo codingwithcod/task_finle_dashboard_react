1996d0406b5349753bbb23ca99e73a85709d7a6e
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-12-29 16:05:26 +0530

    design activity bar chart

--- src/screens/dashboard/dashboardHome/DashboardHome.tsx
+++ src/screens/dashboard/dashboardHome/DashboardHome.tsx
@@ -1,3 +1,9 @@
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import { BsCartCheckFill, BsCartXFill, BsFillCartFill } from "react-icons/bs";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa";
+import { RiMoneyRupeeCircleFill } from "react-icons/ri";
+import ActivityBarChart from "./components/ActivityBarChart";
+
 const DashboardHome = () => {
   return (
     <div className="text-forground">
@@ -7,18 +13,117 @@ const DashboardHome = () => {
         {/* ---> Kpi's */}
         <div className="flex flex-col gap-5 lg:flex-row">
           <div className="grid w-full grid-cols-4 gap-3 lg:w-[65%]">
-            <div className="h-28 rounded-md bg-background"></div>
-            <div className="h-28 rounded-md bg-background"></div>
-            <div className="h-28 rounded-md bg-background"></div>
-            <div className="h-28 rounded-md bg-background"></div>
+            <div className="flex h-28 flex-col justify-between rounded-md bg-background p-3">
+              <div className="flex flex-col gap-1">
+                <div className="flex h-9 w-9 items-center justify-center rounded-md bg-blue-500/20">
+                  <BsFillCartFill className="text-xl text-blue-500" />
+                </div>
+                <p className="text-sm font-medium">Total Orders</p>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-2xl font-bold">75</span>
+                <div className="flex items-end gap-1 font-semibold text-green-500">
+                  <FaCaretUp className="text-xl" />
+                  <span>3%</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex h-28 flex-col justify-between rounded-md bg-background p-3">
+              <div className="flex flex-col gap-1">
+                <div className="flex h-9 w-9 items-center justify-center rounded-md bg-green-500/20">
+                  <BsCartCheckFill className="text-xl text-green-500" />
+                </div>
+                <p className="text-sm font-medium">Total Delivered</p>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-2xl font-bold">70</span>
+                <div className="flex items-end gap-1 font-semibold text-red-500">
+                  <FaCaretDown className="text-xl" />
+                  <span>3%</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex h-28 flex-col justify-between rounded-md bg-background p-3">
+              <div className="flex flex-col gap-1">
+                <div className="flex h-9 w-9 items-center justify-center rounded-md bg-red-500/20">
+                  <BsCartXFill className="text-xl text-red-500" />
+                </div>
+                <p className="text-sm font-medium">Total Cancelled</p>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-2xl font-bold">05</span>
+                <div className="flex items-end gap-1 font-semibold text-green-500">
+                  <FaCaretUp className="text-xl" />
+                  <span>3%</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex h-28 flex-col justify-between rounded-md bg-background p-3">
+              <div className="flex flex-col gap-1">
+                <div className="flex h-9 w-9 items-center justify-center rounded-md bg-purple-500/20">
+                  <RiMoneyRupeeCircleFill className="text-xl text-purple-500" />
+                </div>
+                <p className="text-sm font-medium">Total Revenue</p>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-2xl font-bold">₹70k</span>
+                <div className="flex items-end gap-1 font-semibold text-red-500">
+                  <FaCaretDown className="text-xl" />
+                  <span>3%</span>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="h-28 w-full rounded-md bg-red-500/50 lg:w-[35%]"></div>
+
+          {/* ---> Circular progres box */}
+          <div className="flex h-28 w-full rounded-md bg-background p-3 lg:w-[35%]">
+            <div className="flex h-full w-[60%] flex-col justify-between">
+              <p className="text-sm font-semibold">Net Profit</p>
+              <span className="text-2xl font-extrabold">₹9345.25</span>
+              <div className="flex items-end gap-1 font-semibold text-green-500">
+                <FaCaretUp className="text-xl" />
+                <span>3%</span>
+              </div>
+            </div>
+
+            {/* ---> Circular progres */}
+            <div className="flex w-[40%] flex-col items-center">
+              <div className="relative h-[90%] w-40">
+                <CircularProgressbar
+                  value={70}
+                  strokeWidth={12}
+                  className="h-full w-full"
+                  styles={buildStyles({
+                    strokeLinecap: "round",
+                    textSize: "16px",
+                    pathTransitionDuration: 0.5,
+                    pathColor: `#7095ff`,
+                    textColor: "#fff",
+                    trailColor: "#7095ff40",
+                  })}
+                />
+                <div className="absolute right-16 top-6 flex flex-col text-center">
+                  <span className="text-sm font-bold">70%</span>
+                  <span className="text-[7px] leading-none">Goal</span>
+                  <span className="text-[7px]">Completed</span>
+                </div>
+              </div>
+
+              <p className="mt-[2px] text-[7px]">This is generated report</p>
+            </div>
+          </div>
         </div>
 
         {/* ---> Activity Graph */}
         <div className="flex flex-col gap-5 lg:flex-row">
-          <div className="h-52 w-full rounded-md bg-blue-500/50 lg:w-[65%]"></div>
-          <div className="h-52 w-full rounded-md bg-red-500/50 lg:w-[35%]"></div>
+          {/* ---> Bar chart */}
+          <div className="h-52 w-full rounded-md bg-background lg:w-[65%]">
+            <ActivityBarChart />
+          </div>
+          <div className="h-52 w-full rounded-md bg-background lg:w-[35%]"></div>
         </div>
 
         {/* ---> Recent orders and feedback. */}
---
+++ src/screens/dashboard/dashboardHome/components/ActivityBarChart.tsx
@@ -0,0 +1,91 @@
+import { FC, useState } from "react";
+import { FaCaretDown } from "react-icons/fa";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+
+interface IChartData {
+  name: string;
+  income: number;
+}
+
+const chart = [
+  { name: "1", income: 3000 },
+  { name: "3", income: 3000 },
+  { name: "5", income: 4000 },
+  { name: "7", income: 6000 },
+  { name: "9", income: 5000 },
+  { name: "11", income: 7000 },
+  { name: "13", income: 9000 },
+  { name: "15", income: 8000 },
+  { name: "17", income: 9000 },
+  { name: "19", income: 12000 },
+  { name: "21", income: 11000 },
+  { name: "23", income: 10000 },
+  { name: "25", income: 7000 },
+  { name: "27", income: 9000 },
+  { name: "29", income: 11000 },
+  { name: "31", income: 11000 },
+];
+
+const ActivityBarChart: FC = () => {
+  const [chartData] = useState<IChartData[]>(chart);
+
+  const customYAxisTick = (tick: number) => {
+    return `${tick / 1000}K`;
+  };
+
+  const customTooltip = (tick: number) => {
+    return `₹${tick.toLocaleString()}`;
+  };
+
+  return (
+    <div className="h-full w-full rounded-md bg-background">
+      {/* ---> Bar chart Header  */}
+      <div className="flex h-[20%] w-full justify-between p-3 pt-2">
+        <div>
+          <h2 className="text-semiDarkText font-bold">Activity</h2>
+        </div>
+
+        <div className="text-xxs text-lightText flex flex-col gap-2 sm:flex-row">
+          <div className="flex h-7 w-24 cursor-pointer items-center justify-center gap-1 rounded-full border bg-gray-500/40">
+            <p className="text-sm">Monthly</p>
+            <FaCaretDown />
+          </div>
+        </div>
+      </div>
+
+      {/* ---> Bar chart goes here  */}
+      <div className="flex h-[80%] w-full justify-between">
+        {/* {totalRevenueThisYear > 0 ? ( */}
+        <ResponsiveContainer>
+          <BarChart data={chartData}>
+            <XAxis
+              dataKey="name"
+              fontSize={10}
+              axisLine={false}
+              tickLine={false}
+            />
+            <YAxis
+              fontSize={14}
+              axisLine={false}
+              tickLine={false}
+              tickFormatter={customYAxisTick}
+            />
+            <Tooltip
+              formatter={customTooltip}
+              contentStyle={{ background: "black" }}
+            />
+
+            <Bar
+              dataKey="income"
+              fill="#7095ff"
+              barSize={15}
+              radius={20}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+export default ActivityBarChart;
