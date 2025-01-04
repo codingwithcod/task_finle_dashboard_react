bcaa74af94824c2b5c9e60aeb85123b77fcd15ed
TheAbhiPatel <theabhipatel.co@gmail.com>
2025-01-04 10:18:56 +0530

    chore change name in home screen and package.json file

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <section className="flex h-screen flex-col items-center justify-center bg-gradient-to-br from-[#030138] to-[#000000] text-center">
       <div>
         <h1 className="text-2xl font-bold text-white sm:text-5xl">
-          Finle Dashboard <span className="text-lg font-semibold sm:text-2xl">by</span>
+          Dashboard <span className="text-lg font-semibold sm:text-2xl">by</span>
         </h1>
         <h1 className="bg-gradient-to-r from-orange-700 via-orange-500 to-yellow-500 bg-clip-text text-5xl font-bold text-transparent sm:text-8xl">
           TheAbhiPatel
